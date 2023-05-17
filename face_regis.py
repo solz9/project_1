a94c28ba016027c0b5717bcbf5a5dc8ca021b8cf
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-17 19:35:47 +0700

    Update face_regis.py

--- face_regis.py
+++ face_regis.py
@@ -4,32 +4,13 @@ import pandas as pd
 import numpy as np
 import face_recognition
 from deta import Deta
+from encoding import detabase, find_encode_list, get_all_names
 
 
 def detabase():
     DETA_KEY = st.secrets["DETA_KEY"]
     deta = Deta(DETA_KEY) 
     return deta.Base("face_reg_project")
-
-def get_all_names():
-    items = base.fetch().items
-    names = [item["key"] for item in items]
-    if len(names) > 0:
-        return names
-    else:
-        return []
-    
-# LIST CHỨA FACE_ENCODING CỦA NHỮNG GƯƠNG MẶT ĐÃ ĐĂNG KÝ
-def find_encode_list():
-    encode_list = []
-    names = get_all_names()
-    if len(names) > 0:
-        for i in range(len(names)):
-            name_and_encode = base.get(names[i])
-            encode_list.append(name_and_encode['pic'])
-            return encode_list
-    else:
-        return None
 
 base = detabase()
 
