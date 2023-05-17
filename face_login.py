22a20c78fb10c36f4d59052e22ab167150c46950
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-17 18:18:21 +0700

    Update face_login.py

--- face_login.py
+++ face_login.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import pandas as pd
 from deta import Deta
-import face_recognition_models as face_recognition
+import face_recognition
 from encoding import find_encode_list, get_all_names, detabase
 # from face_regis import face_input_regis
 
