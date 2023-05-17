fd9b648949b43293b411c24744b7edc052d4549c
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-17 20:16:49 +0700

    Update password_login.py

--- password_login.py
+++ password_login.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from login import hs_login, gv_login
 
-st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 4')
+st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 0')
 option = st.selectbox('Bạn là?', ('Học sinh', 'Giáo viên'))
 
 if option == 'Giáo viên':
