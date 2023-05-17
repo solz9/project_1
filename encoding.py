4cdc45c0e7a58ec0c09d377d1657c877582d2a61
solz9 <125681221+solz9@users.noreply.github.com>
2023-05-17 19:42:43 +0700

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from deta import Deta
 
-def detabase():
-    DETA_KEY = "c0qy5dgedq2_7aSU1pPYRdDoNvwmqdwwVUDZLUGz3mpU"
-    deta = Deta(DETA_KEY) 
-    return deta.Base("face_reg_project")
-base = detabase()
+
+DETA_KEY = "c0qy5dgedq2_7aSU1pPYRdDoNvwmqdwwVUDZLUGz3mpU"
+deta = Deta(DETA_KEY) 
+data = deta.Base("face_reg_project")
+
 
 def get_all_names():
-    items = base.fetch().items
+    items = data.fetch().items
     names = [item["key"] for item in items]
     if len(names) > 0:
         return names
@@ -21,7 +21,7 @@ def find_encode_list():
     names = get_all_names()
     if len(names) > 0:
         for i in range(len(names)):
-            name_and_encode = base.get(names[i])
+            name_and_encode = data.get(names[i])
             encode_list.append(name_and_encode['pic'])
             return encode_list
     else:
